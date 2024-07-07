7d1801415d58998c32ac897a7f6a977e94cd1ca4
Nishi-1805 <165827696+Nishi-1805@users.noreply.github.com>
2024-07-07 17:28:59 +0530

    Update meeting.js

--- meeting.js
+++ meeting.js
@@ -1,38 +1,35 @@
-// app.js
 const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
 const sequelize = require('./util/database');
-const timeSlotRoutes = require('./routes/timeslot');
-const bookingRoutes = require('./routes/bookingslot');
-
+const path = require('path');
+const TimeSlot = require('./models/Timeslot');
+const Booking = require('./models/booking')
+const TimeRouter = require('./routes/timeslot');
+const bookingRouter = require('./routes/bookingslot');
 const app = express();
-
-// Middleware
+const seedTimeSlots = require('./timeslots');
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'views')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'availableslots.html'));
+  res.sendFile(path.join(__dirname, 'views', 'availableslots.html'));
 });
 
-// Routes
-app.use('/', timeSlotRoutes);
-app.use('/api', bookingRoutes);
+app.use('/', TimeRouter);
+app.use('/api', bookingRouter);
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
 
-// Start server and database connection
+// Sync the models with the database
 sequelize.sync()
-    .then(() => {
-        console.log('Database synced');
-        app.listen(3000, () => {
-            console.log(`Server running on port 3000`);
-        });
-    })
-    .catch(err => console.error('Error syncing database:', err));
+  .then(() => {
+    console.log('Database schema synchronized');
+    //seedTimeSlots();
+    app.listen(3000, () => {
+      console.log('Server listening on port 3000');
+    });
+  })
+  .catch((error) => {
+    console.error('Error synchronizing database schema:', error);
+  });
+
+  
