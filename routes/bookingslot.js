61afbf1ec6a488453bdcfe49eb35f2c9014dbacb
Nishi-1805 <165827696+Nishi-1805@users.noreply.github.com>
2024-07-08 15:29:12 +0530

    Update bookingslot.js

--- routes/bookingslot.js
+++ routes/bookingslot.js
@@ -4,6 +4,6 @@ const bookingController = require('../controllers/bookings');
 
 router.post('/book-time-slot', bookingController.bookTimeSlot);
 router.delete('/cancel-booking/:id', bookingController.deleteBooking);
-router.get('/get-booked-time-slots', bookingController.getBookedTimeSlots); // Add this route
+router.get('/get-booked-time-slots', bookingController.getBookedTimeSlots); 
 
 module.exports = router;
