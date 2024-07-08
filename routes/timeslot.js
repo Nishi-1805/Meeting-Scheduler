cfe6458539d0d6e16231df7a3dce93962070304b
Nishi-1805 <165827696+Nishi-1805@users.noreply.github.com>
2024-07-08 13:56:31 +0530

    Update timeslot.js

--- routes/timeslot.js
+++ routes/timeslot.js
@@ -6,4 +6,5 @@ const timeSlotController = require('../controllers/timeSlotController');
 router.get('/time-slots', timeSlotController.getTimeSlots);
 router.post('/time-slots', timeSlotController.createTimeSlot);
 router.put('/time-slots/:time', timeSlotController.updateAvailableSlots);
+router.get('/get-available-slots/:time', timeSlotController.getAvailableSlots);
 module.exports = router;
